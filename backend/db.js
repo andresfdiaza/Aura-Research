eb1df6843b60db8405c4305349fa2ee89b6d7336
Andres Diaz <andres.diaz@ubuntusrv>
2026-03-27 09:09:54 -0500

    Subida inicial de todo el proyecto Aura

--- backend/db.js
+++ backend/db.js
@@ -1,7 +1,9 @@
 const mysql = require('mysql2/promise');
 require('dotenv').config();
 
-const pool = mysql.createPool({
+
+// Pool para la base de datos local
+const localPool = mysql.createPool({
   host: process.env.DB_HOST || 'localhost',
   user: process.env.DB_USER || 'root',
   password: process.env.DB_PASS || 'Amaamama12345.',
@@ -11,4 +13,18 @@ const pool = mysql.createPool({
   queueLimit: 0,
 });
 
-module.exports = pool;
+// Pool para la base de datos del servidor remoto
+const remotePool = mysql.createPool({
+  host: process.env.REMOTE_DB_HOST || 'ingenieria.unac.edu.co', // Cambia esto por la IP o dominio del servidor si es necesario
+  user: process.env.REMOTE_DB_USER || 'investiga',
+  password: process.env.REMOTE_DB_PASS || 'Amaamama12345',
+  database: process.env.REMOTE_DB_NAME || 'scraping',
+  waitForConnections: true,
+  connectionLimit: 10,
+  queueLimit: 0,
+});
+
+module.exports = {
+  localPool,
+  remotePool
+};
